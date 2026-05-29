5313fb909dc88340263494f1d39b9d150ff9f2eb
Sini <nicolasromerosalazar@gmail.com>
2026-05-28 21:23:15 -0300

    Modificacion universidad

--- AD2/src/TPJAVA/domain/Universidad.java
+++ AD2/src/TPJAVA/domain/Universidad.java
@@ -1,79 +1,89 @@
-package TPJAVA.domain;
+/**
+ * YA QUE VAMOS A TENER QUE USAR UNA INTERFAZ GRAFICA (Swing es lo mas facil) PARA MOSTRAR VAMOS A TENER QUE USAR
+ * METODOS QUE DEVUELVAN STRINGS. CON EL FORMATO "System.out.println(...)" SOLO PODEMOS MOSTRAR EN LA CONSOLA.
+ */
 
+package TPJAVA.domain; //ESTABLECE UBICACION EN "domain"
+
+//ACCEDE A CLASES "Asignatura" y "Inscripcion"
 import TPJAVA.domain.asignaturas.Asignatura;
 import TPJAVA.domain.inscripciones.Inscripcion;
 
+//ACCEDE A LIBRERIAS
 import java.util.Iterator;
 import java.util.LinkedList;
 
-public class Universidad {
-    private LinkedList<Asignatura> asignaturas;
-
+public class Universidad { //CLASE CONTROLADORA UNIVERSIDAD
+    private LinkedList<Asignatura> asignaturas; //LISTA DE ASIGNATRAS
 
     Universidad(){
         asignaturas = new LinkedList<>();
-    }
+    } //CONSTRUCTOR DE CLASE Universidad
     public void agregarAsignatura(String cod, String nombre, boolean promocionable, int cuatrimestre, char tipo, int clasesTotales){
-        Asignatura asignatura = new Asignatura(cod,nombre,promocionable,cuatrimestre,tipo, clasesTotales);
-        asignaturas.add(asignatura);
+        Asignatura asignatura = new Asignatura(cod,nombre,promocionable,cuatrimestre,tipo, clasesTotales); //CREA OBJETO asignatura
+        asignaturas.add(asignatura); //AÑADE OBJETO asignatura A LA LISTA
     }
 
     public void detalleCatedraLibres(String cod, int C1, int C2){
         System.out.println("Detalle de los alumnos libres%n");
 
-        Iterator<Asignatura> it = asignaturas.iterator();
+        Iterator<Asignatura> it = asignaturas.iterator(); //DECLARAMOS ITERADOR PARA RECORRER LISTA DE ASIGNATURAS
+        /// Asignatura AsignaturaActual = it.hasNext() ? it.next() : null; // la lista esta vacia? si esta vacia asignamos null, si no la cabeza
+        /// while (AsignaturaActual != null) {
+        while(it.hasNext()) { //AÑADIDO - SINI
+            Asignatura AsignaturaActual = it.next(); //MOVIDO - SINI
+            int cuatrimestre = AsignaturaActual.getCuatrimestre(); //AÑADIDO - SINI
+            if(cuatrimestre >= C1 && cuatrimestre <= C2){ //EDITADO - SINI
 
-        Asignatura AsignaturaActual = it.hasNext() ? it.next() : null; // la lista esta vacia? si esta vacia asignamos null, si no la cabeza
-
-        while (AsignaturaActual != null) {
-            if(AsignaturaActual.getCuatrimestre() >= C1 && AsignaturaActual.getCuatrimestre() <= C2){
-                Iterator <Inscripcion> itAsig = AsignaturaActual.getInscripciones().iterator();
-                Inscripcion inscripcionActual = itAsig.hasNext() ? itAsig.next() : null;
-
-                while (inscripcionActual != null){
+                Iterator<Inscripcion> itAsig = AsignaturaActual.getInscripciones().iterator(); //ITERADOR PARA RECORRER LISTA DE INSCRIPCIONES
+                /// Inscripcion inscripcionActual = itAsig.hasNext() ? itAsig.next() : null;
+                /// while (inscripcionActual != null){
+                while (itAsig.hasNext()) { //EDITADO - SINI
+                    Inscripcion inscripcionActual = itAsig.next(); //MOVIDO - SINI
                     if(inscripcionActual.Condicion().equals("Libre")){
                         inscripcionActual.getAlumno().muestra();
-                        System.out.println("%n");
+                        System.out.println("%n"); //POSIBLE CORRECION - CADA VEZ QUE MUESTRA, YA REALIZA EL SALTO DE LINEA YA QUE ES ln
                     }
-                    inscripcionActual = itAsig.hasNext() ? itAsig.next() : null;
+                    /// inscripcionActual = itAsig.hasNext() ? itAsig.next() : null;
                 }
-
-
             }
-            AsignaturaActual = it.hasNext() ? it.next() : null; // es el ultimo? si es el ultimo, asignamos null al sig, si no, seguimos buscando
+            /// AsignaturaActual = it.hasNext() ? it.next() : null; // es el ultimo? si es el ultimo, asignamos null al sig, si no, seguimos buscando
         }
     }
 
-    public void detalleCatedra(String cod){
-        Iterator<Asignatura> it = asignaturas.iterator();
-        Asignatura asignaturaActual = it.hasNext() ? it.next() : null; // la lista esta vacia? si esta vacia asignamos null, si no la cabeza
+    public String detalleCatedra(String cod){
+        Iterator<Asignatura> it = asignaturas.iterator(); //ITERADOR DE LA LISTA asignaturas
+        /// Asignatura asignaturaActual = it.hasNext() ? it.next() : null; // la lista esta vacia? si esta vacia asignamos null, si no la cabeza
+        Asignatura asignaturaActual = null; //EDITADO - SINI
+        boolean encontre=false; //AÑADIDO - SINI
 
-        while (asignaturaActual != null && !asignaturaActual.getCod().equals(cod)) {
-            asignaturaActual = it.hasNext() ? it.next() : null; // es el ultimo? si es el ultimo, asignamos null al sig, si no, seguimos buscando
+        /// while (asignaturaActual != null && !asignaturaActual.getCod().equals(cod)) {
+        while (it.hasNext() && !encontre) { //EDITADO - SINI
+            /// asignaturaActual = it.hasNext() ? it.next() : null; // es el ultimo? si es el ultimo, asignamos null al sig, si no, seguimos buscando
+            asignaturaActual = it.next(); //EDITADO - SINI
+            if (asignaturaActual.getCod().equals(cod))
+                encontre = true; //AÑADIDO - SINI
         }
 
-        if (asignaturaActual != null) { // lo encontramos?
-            Iterator<Inscripcion> itAsig = asignaturaActual.getInscripciones().iterator();
-            Inscripcion inscripcionActual = itAsig.hasNext() ? itAsig.next(): null;
+        if (encontre) { // lo encontramos? //EDITADO(lo cambie por una flag) - SINI
+            /// Iterator<Inscripcion> itAsig = asignaturaActual.getInscripciones().iterator();
+            /// Inscripcion inscripcionActual = itAsig.hasNext() ? itAsig.next(): null;
+            StringBuilder reporte = new StringBuilder();
 
-            System.out.println("Nombre de la asignatura: " + asignaturaActual.getNombre() +"\n");
-            while(inscripcionActual != null){ // listamos todos los alumnos con sus clases asistidas
-                System.out.println("Datos del alumno:\n");
-                System.out.printf("\t");
-                inscripcionActual.getAlumno().muestra();
-                System.out.println();
-
-                System.out.printf("\t");
-                inscripcionActual.muestraClases();
-
-                System.out.println("\tPorcentaje de asistencia: "+ inscripcionActual.getAsistencias()/asignaturaActual.getClasesTotales() * 100);
-                System.out.println("\tModalidad: " + inscripcionActual.Modalidad());
-                System.out.println("\tCondicion Academica: " + inscripcionActual.Condicion());
-
-                inscripcionActual = itAsig.hasNext() ? itAsig.next() : null;
+            reporte.append("Nombre de la asignatura: ").append(asignaturaActual.getNombre());
+            /// while(inscripcionActual != null){ // listamos todos los alumnos con sus clases asistidas
+            for (Inscripcion inscripcionActual : asignaturaActual.getInscripciones()) { //AÑADIDO - SINI
+                reporte.append("Datos del alumno: ");
+                reporte.append(inscripcionActual.getAlumno().muestra());
+                reporte.append(inscripcionActual.muestraClases());
+                reporte.append("Porcentaje de asistencia: ").append(inscripcionActual.getAsistencias()/asignaturaActual.getClasesTotales() * 100);
+                reporte.append("Modalidad: ").append(inscripcionActual.Modalidad());
+                reporte.append("Condicion Academica: ").append(inscripcionActual.Condicion());
+                /// inscripcionActual = itAsig.hasNext() ? itAsig.next() : null;
             }
-        } else {
-            System.out.println("La asignatura no existe");
-        }
+            return reporte.toString();
+        } else
+            return "La asignatura no existe";
+
     }
 }
