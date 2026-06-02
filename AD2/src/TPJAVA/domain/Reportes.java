29602c0cbd99f7f82e8dc808d89b9a7f25fc96a8
lucianovidal8 <lucianovidalmiami@gmail.com>
2026-06-01 21:49:29 -0300

    Creamos clase de reportes, creamos exceptions en asignaturas y corregimos errores

--- AD2/src/TPJAVA/domain/Reportes.java
+++ AD2/src/TPJAVA/domain/Reportes.java
@@ -9,36 +9,33 @@ import java.util.Iterator;
 public class Reportes {
 
 
-    public void detalleAsignatura(String cod, Universidad universidad){ // luego con swing ver como mostrarlo
+    public static void detalleAsignatura(String cod, Universidad universidad) throws NoEncuentraAsignaturaException{ // luego con swing ver como mostrarlo
         StringBuilder sb = new StringBuilder();
         Asignatura asignaturaActual = null;
-        try{
-            asignaturaActual = universidad.encuentraAsignatura(cod);
-            Iterator<Inscripcion> itInsc= asignaturaActual.getInscripciones().iterator();
-            sb.append("Nombre de la asignatura: ").append(asignaturaActual.getNombre()).append("\n");
-            while(itInsc.hasNext()){
-                Inscripcion inscripcionActual = itInsc.next();
+        asignaturaActual = universidad.encuentraAsignatura(cod);
+        Iterator<Inscripcion> itInsc= asignaturaActual.getInscripciones().iterator();
+        sb.append("Nombre de la asignatura: ").append(asignaturaActual.getNombre()).append("\n");
+        while(itInsc.hasNext()){
+            Inscripcion inscripcionActual = itInsc.next();
 
-                sb.append("Datos del alumno:\n\t").append(inscripcionActual.getAlumno().muestra());
-                sb.append("\n");
+            sb.append("Datos del alumno:\n\t").append(inscripcionActual.getAlumno().muestra());
+            sb.append("\n");
 
-                sb.append("\t");
-                sb.append(inscripcionActual.muestraClases());
+            sb.append("\t");
+            sb.append(inscripcionActual.muestraClases());
 
-                sb.append("\tPorcentaje de asistencia: ").append(inscripcionActual.getAsistencias()/asignaturaActual.getClasesTotales() * 100);
-                sb.append("\tModalidad: ").append(inscripcionActual.Modalidad());
-                sb.append("\tCondicion Academica: ").append(inscripcionActual.Condicion());
-            }
+            sb.append("\tPorcentaje de asistencia: ").append(inscripcionActual.getAsistencias()/asignaturaActual.getClasesTotales() * 100);
+            sb.append("\tModalidad: ").append(inscripcionActual.Modalidad());
+            sb.append("\tCondicion Academica: ").append(inscripcionActual.Condicion());
         }
-        catch(NoEncuentraAsignaturaException e){
-            sb.append("");
-        }
+
     }
 
-    public void detalleCatedraLibres(Universidad universidad, String cod, int C1, int C2){
+
+    public static void detalleCatedraLibres(Universidad universidad, String cod, int C1, int C2)throws NoEncuentraAsignaturaException{
         StringBuilder sb = new StringBuilder();
         sb.append("Detalle de los alumnos libres\n");
-        try{
+
             Asignatura asignaturaActual = universidad.encuentraAsignatura(cod);
             int cuatrimestre = asignaturaActual.getCuatrimestre();
             if(cuatrimestre >= C1 && cuatrimestre <= C2){
@@ -49,13 +46,12 @@ public class Reportes {
                         sb.append(inscripcionActual.getAlumno().muestra()).append("\n");
                 }
             }
-        }catch(NoEncuentraAsignaturaException e){
-            sb.append("");
         }
 
-    }
 
-    public void rankingPresentismo(Universidad universidad){
+
+    public void rankingPresentismo(Universidad universidad) throws NoEncuentraAsignaturaException{
         // desarrollar
     }
+
 }
